86608fda8c8d98260d3b1f8b747361c9b2b2452c
Mitch <Mitch@Mitch-LAPTOP-Lenovo>
2017-09-04 14:40:29 -0500

    Converter Start

--- src/converter/Binary.java
+++ src/converter/Binary.java
@@ -6,12 +6,21 @@ package converter;
  * @author Mitch
  */
 public class Binary {
+    
     private int BIN;
+    
     
     public Binary(int BIN){
         setBIN(BIN);   
     
     }
     
+    public int getBIN() {
+        return BIN;
+    }
+
+    public void setBIN(int BIN) {
+        this.BIN = BIN;
+    }
     
 }
--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -1,17 +1,21 @@
-
 package converter;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Mitch
  */
 public class Converter {
+    
+    Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Binary BIN = new Binary();
         System.out.println("Welcome to the converter. \n" +
                            "Please enter a number to convert to binary." ); 
-        
-        
+        Binary BIN = new BIN();
+        Bin = Integer.parseInt(keyboard.nextLine());
         
     }
     
